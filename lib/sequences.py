bd946dc78a05b5fc6b74824f041d03356010c9da
Red <red.sarouf2014@gmail.com>
2023-07-09 21:53:40 -0400

    add python sequences lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    i = 0
+    j = 1
+    if length > 0:
+        fibonacci_list.append(i)
+    if length > 1:
+        fibonacci_list.append(j)
+    if length > 2:
+        while length > 2:
+            fibonacci_list.append(
+                fibonacci_list[i - j] + fibonacci_list[i - (j + 1)])
+            length -= 1
+    print(fibonacci_list)
